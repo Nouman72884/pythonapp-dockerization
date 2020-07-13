bba57cf704c5cfb7e69ec2ca7c97b2b32a601b71
Nouman72884 <noumansatti7@gmail.com>
2020-07-13 21:48:03 +0500

    demo files

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,16 +1,13 @@
 multibranchPipelineJob('demo') {
     branchSources {
         git {
-            //id('123456789') // IMPORTANT: use a constant and unique identifier
+
             remote('https://github.com/Nouman72884/pythonapp-dockerization.git')
             credentialsId('github')
             includes('master app-changes')
         }
     }
-    triggers {
-        periodic(1)
-
-}
+    
     orphanedItemStrategy {
         discardOldItems {
             numToKeep(20)
